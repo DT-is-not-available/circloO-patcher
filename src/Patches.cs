7a999b5cb6086489161bbc57aaedd7d0f761a849
qaptivator <ac@i-ns.net>
2024-05-31 15:09:29 +0300

    merged CircloOPatch and Patches

--- src/Patches.cs
+++ src/Patches.cs
@@ -7,6 +7,31 @@ using System.Xml.Linq;
 
 namespace cpatcher;
 
+public class CircloOPatch
+{
+    public Guid Id { get; init; }
+    public string Name { get; init; } // aka internal name, init makes it so it cant be changes afterwards
+    public string DisplayName { get; set; }
+    public string Description { get; set; }
+    public Action Callback { get; set; }
+    public bool Required { get; set; }
+
+    public CircloOPatch(string Name, string DisplayName, string Description, Action Callback, bool Required = false)
+    {
+        this.Id = Guid.NewGuid();
+        this.Name = Name;
+        this.DisplayName = DisplayName;
+        this.Description = Description;
+        this.Callback = Callback;
+        this.Required = Required;
+    }
+
+    public void Execute()
+    {
+        this.Callback(); // pass some additional arguments in like data and helper functions
+    }
+}
+
 public partial class MainWindow
 {
     public List<CircloOPatch> Patches { get; set; } = new List<CircloOPatch>();
